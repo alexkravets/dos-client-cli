977ee2e2c641cc3e934e17db367e857141036932
Oleksandr Kravets <alex@slatestudio.com>
2024-12-05 15:37:11 +0200

    Update enum label titlize method

--- src/getInputOptions.js
+++ src/getInputOptions.js
@@ -21,8 +21,11 @@ const getInputOptions = ({
   const componentPath = items.$ref || $ref
   const { enum: enumValues } = getComponentByPath(operation, componentPath)
 
+  const titlizeEnumValue = value =>
+    value.split('_').map(word => capitalize(word)).join(' ')
+
   const options = enumValues.map(value => ({
-    label: capitalize(value),
+    label: titlizeEnumValue(value),
     value
   }))
 
